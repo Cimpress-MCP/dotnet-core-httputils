4e21e87de2a8d155d7f1d2766f1ae097dbbe4e38
Markus Thurner <mthurner@cimpress.com>
2016-07-17 23:44:32 +0200

    Provide a way to override the image format of the provided file and make internally used extension methods publicly available.

--- src/Cimpress.Extensions.Http/DataUriConversion.cs
+++ src/Cimpress.Extensions.Http/DataUriConversion.cs
@@ -14,10 +14,11 @@ namespace Cimpress.Extensions.Http
         /// </summary>
         /// <param name="url">The url where the image is located.</param>
         /// <param name="logger">A logger.</param>
-        /// <param name="fileInfo">A FileInfo to retrieve the local fallback image.</param>
+        /// <param name="fallbackFileInfo">A FileInfo to retrieve the local fallback image.</param>
+        /// <param name="fallbackMediaType">The media type of the fallback image.</param>
         /// <param name="messageHandler">An optional message handler.</param>
         /// <returns>A string that contains the data uri of the downloaded image, or a default image on any error.</returns>
-        public static async Task<string> DownloadImageAndConvertToDataUri(this string url, ILogger logger, IFileInfo fileInfo, HttpMessageHandler messageHandler)
+        public static async Task<string> DownloadImageAndConvertToDataUri(this string url, ILogger logger, IFileInfo fallbackFileInfo, string fallbackMediaType = "image/png", HttpMessageHandler messageHandler = null)
         {
             // exclude completely invalid URLs
             if (!string.IsNullOrWhiteSpace(url))
@@ -45,23 +46,43 @@ namespace Cimpress.Extensions.Http
             }
 
             // any error or invalid URLs just return the default data uri
-            return await DefaultPreviewToDataUri(fileInfo);
+            return await fallbackFileInfo.ToDataUri(fallbackMediaType);
         }
 
-        private static async Task<string> DefaultPreviewToDataUri(IFileInfo fileInfo)
+        /// <summary>
+        /// Converts a file info to a data URL.
+        /// </summary>
+        /// <param name="fileInfo">The fileInfo that points to a file which content should be converted to a Data URI format.</param>
+        /// <param name="mediaType">The media type of the file.</param>
+        /// <returns>A string that contains the data uri of the provided file content.</returns>
+        public static Task<string> ToDataUri(this IFileInfo fileInfo, string mediaType)
+        {
+            return fileInfo.CreateReadStream().ToDataUri(mediaType);
+        }
+
+        /// <summary>
+        /// Converts a stream to a data URL.
+        /// </summary>
+        /// <param name="stream">The stream which content should be converted to a Data URI format.</param>
+        /// <param name="mediaType">The media type of the stream.</param>
+        /// <returns>A string that contains the data uri of the stream's content.</returns>
+        public static async Task<string> ToDataUri(this Stream stream, string mediaType)
         {
             // copy to memory stream and convert the bytes to a base64 encoded string
             using (var ms = new MemoryStream())
             {
-                using (var stream = fileInfo.CreateReadStream())
-                {
-                    await stream.CopyToAsync(ms);
-                }
-                return ms.ToArray().ToDataUri("image/png");
+                await stream.CopyToAsync(ms);
+                return ms.ToArray().ToDataUri(mediaType);
             }
         }
 
-        private static string ToDataUri(this byte[] data, string mediaType)
+        /// <summary>
+        /// Converts a byte array to a data URL.
+        /// </summary>
+        /// <param name="data">The data that should be converted.</param>
+        /// <param name="mediaType">The media type of the content.</param>
+        /// <returns>A string that contains the data uri of the provided data.</returns>
+        public static string ToDataUri(this byte[] data, string mediaType)
         {
             return $"data:{mediaType};base64,{Convert.ToBase64String(data)}";
         }
---
+++ test/Cimpress.Extensions.Http.UnitTests/DataUriConversion_when_converting_to_base_64.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.FileProviders;
+using FluentAssertions;
+using Moq;
+using Xunit;
+using System.Linq;
+
+namespace Cimpress.Extensions.Http.UnitTests
+{
+    public class DataUriConversion_when_converting_to_base_64
+    {
+        private const string rawDataToConvert = "unittest";
+        private const string rawDataBase64 = "dW5pdHRlc3Q=";
+
+        [Fact]
+        public async Task Converts_fileinfo_to_datauri()
+        {
+            // setup
+            var stream = SetupMemoryStream();
+            var fileInfo = new Mock<IFileInfo>(MockBehavior.Strict);
+            fileInfo.Setup(s => s.CreateReadStream()).Returns(stream);
+
+            // execute
+            var result = await fileInfo.Object.ToDataUri("image/unittest");
+
+            // verify
+            VerifyResult(result);
+        }
+
+        [Fact]
+        public async Task Converts_stream_to_datauri()
+        {
+            // setup
+            var stream = SetupMemoryStream();
+
+            // execute
+            var result = await stream.ToDataUri("image/unittest");
+
+            // verify
+            VerifyResult(result);
+        }
+
+        [Fact]
+        public void Converts_byte_to_datauri()
+        {
+            // setup
+            byte[] data = rawDataToConvert.ToCharArray().Select(Convert.ToByte).ToArray();
+
+            // execute
+            var result = data.ToDataUri("image/unittest");
+
+            // verify
+            VerifyResult(result);
+        }
+
+        private static MemoryStream SetupMemoryStream()
+        {
+            byte[] data = rawDataToConvert.ToCharArray().Select(Convert.ToByte).ToArray();
+            var stream = new MemoryStream(data);
+            return stream;
+        }
+
+        private void VerifyResult(string result)
+        {
+            var expected = $"data:image/unittest;base64,{rawDataBase64}";
+            result.Should().Be(expected);
+        }
+    }
+}
